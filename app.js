09595b063cc73d4c8e04fe02f77007a9103b7247
Rahul Kandalkar <rahul.kandalkar@simplifyvms.com>
2025-12-04 10:04:53 +0530

    commit changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const startServer = async () => {
 
         const PORT = process.env.PORT || 8009;
 
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`Server is running on port: ${PORT}`);
         });
     } catch (err) {
--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,24 @@
 const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 const sequelize = new Sequelize(
-
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: false,
-    }
-
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,  
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000, 
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.join(__dirname, '../certs/ca.pem')),
+        require: true,        
+      },
+    },
+  }
 );
 
 module.exports = sequelize;
